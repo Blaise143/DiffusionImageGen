970ac691dabc955a1fea66ea9a11bf51c782fd12
Blaise143 <blaiseappol@gmail.com>
2025-03-18 12:30:18 +0100

    Added time embedding to unet

--- models/unet.py
+++ models/unet.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
-import torch.functional as F
+import math
 
 class Unet(nn.Module):
-    def __init__(self, in_channels: int=3, out_channels: int=3):
+    def __init__(self, in_channels: int=3, out_channels: int=3, time_embeddig_dim: int=64):
         super().__init__()
         self.down1 = DownSample(in_channels, 32)
         self.down2 = DownSample(32, 64)
@@ -21,14 +21,27 @@ class Unet(nn.Module):
         self.up3 = UpSample(64, 32, skip_channels=32)
 
         self.out = nn.Conv2d(in_channels=32,out_channels=out_channels, kernel_size=1)
+        self.mlp = nn.Sequential(
+            nn.Linear(time_embeddig_dim, 264),
+            nn.ReLU(),
+            nn.Linear(264, 264),
+            nn.ReLU()
+        )
+        self.time_embedding_dim = time_embeddig_dim
 
 
-    def forward(self, x: torch.tensor):
+    def forward(self, x: torch.Tensor, timestep: int):
+
+        time_embedding = sinusoidal_embedding(t = timestep, dim= self.time_embedding_dim).unsqueeze(0).repeat(x.shape[0],1)
+
+        time_embedding = self.mlp(time_embedding).unsqueeze(-1).unsqueeze(-1)
+
         x1, skip1 = self.down1(x)
         x2, skip2 = self.down2(x1)
         x3, skip3 = self.down3(x2)
-
-        x = self.bottleneck(x3)
+        print(f"time_embedding shape: {time_embedding.shape}")
+        x = self.bottleneck(x3)+time_embedding
+        print(f"bottleneck dim: {x.shape}")
 
         x = self.up1(x, skip3)
         x = self.up2(x, skip2)
@@ -36,11 +49,12 @@ class Unet(nn.Module):
         return self.out(x)
 
 class DownSample(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, kernel_size: int=3, stride: int=1, pool: int=2, activation = nn.ReLU()):
+    def __init__(self, in_channels: int, out_channels: int,
+                 kernel_size: int=3, stride: int=1, pool: int=2,
+                 activation = nn.ReLU()):
         super().__init__()
         self.convs = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding=kernel_size//2),
-            # nn.MaxPool2d(pool),
             activation,
             nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding=kernel_size // 2),
         )
@@ -63,21 +77,37 @@ class UpSample(nn.Module):
 
     def forward(self, x: torch.Tensor, skip: torch.Tensor):
         x = self.upconv(x)
-        # print(f"x shape: {x.shape}, skip shape: {skip.shape}")
         x = torch.cat([x, skip], dim=1)
         return self.conv(x)
 
+
+def sinusoidal_embedding(t: int, dim: int):
+    if not isinstance(t, torch.Tensor):
+        t = torch.tensor([t])
+    else:
+        t = t.float()
+    t = t.unsqueeze(1) if t.dim() == 1 else t
+    assert dim % 2 == 0, "Embedding dim not even"
+    half_dim = dim // 2
+    exp = -math.log(10000) * torch.arange(half_dim, dtype=torch.float32) / (half_dim - 1)
+    emb = t * torch.exp(exp)
+    emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1)
+    if emb.shape[0] == 1:
+        emb = emb.squeeze(0)
+    return emb
+
+
 if __name__ =="__main__":
+
     x = torch.randn(30, 3, 256, 256)
-    # cnn = DownSample(3,20, 5, 3,2)
-    # up_cnn = UpSample(in_channels=20, out_channels=3, kernel_size=5, stride=3, pool=2)
-    # down = cnn(x)
-    # up = up_cnn(down)
-    # print(out)
-    # print(down.shape)
-    # print(up.shape)
-    # x = torch.randn(1, 3, 256, 256)
+    # y = torch.tensor(3.)
+    # print(x.float())
+    m = sinusoidal_embedding(3, 40)
+    # print(m.shape)
     model = Unet(in_channels=3, out_channels=3)
-    # print(output)
-    output = model(x)
+    output = model(x, 2)
     print(output.shape)
+    # a = torch.tensor(3.).unetsqueeze(-1)
+    # print(a)
+    # print(a.unsqueeze(-1))
+    # print(torch.zeros((3,)).unsqueeze(0).repeat(a.shape[0],1))
